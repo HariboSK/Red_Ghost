a6cc57edbde711377cee6298ea9254053b7f293c
HariboSK <241761700+HariboSK@users.noreply.github.com>
2026-03-29 13:31:31 +0200

    Refactor .htaccess rules, update testimonial button color, clean up home.php, and remove empty log file

--- public/home.php
+++ public/home.php
@@ -4,7 +4,6 @@ require_once dirname(__DIR__) . '/app/core/function.php';
 app_register_error_handlers();
 require_once dirname(__DIR__) . '/app/core/router.php';
 
-
 //toto musi byt posledne lebo sa to testuje error handlerom
 $router = new Router();
 $router->dispatch();
